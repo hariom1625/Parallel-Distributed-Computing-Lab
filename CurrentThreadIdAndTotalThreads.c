1f0a24c85f452b9c29076a2da65935c9e1842524
Hari Om Yadav <66957239+hariom1625@users.noreply.github.com>
2022-01-21 19:33:32 +0530

    Add files via upload

---
+++ CurrentThreadIdAndTotalThreads.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main (int argc, char** argv) {
+
+	int intermediateSum = 0, sum = 0, n;
+
+	printf("Total Number of Threads present are: %d\n", omp_get_max_threads());
+
+	#pragma omp parallel private(intermediateSum) shared(sum,n)
+	{
+		intermediateSum = 0;
+		sum = 0;
+		n = 10;
+		#pragma omp for
+		for (int i = 1; i <= n; i++) {
+			intermediateSum += i;
+
+		}
+
+		printf("Intermediate Sum = %d ThreadId = %d\n", intermediateSum, omp_get_thread_num());
+
+		#pragma omp critical
+		{
+			sum += intermediateSum;
+		}
+
+
+	}
+
+	printf("Sum = %d\n", sum );
+	return 0;
+
+}
